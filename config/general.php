e97ecabb00cc90f61f6f7f93f815f789f084131b
Personal Profile <davina.leong.sy@gmail.com>
2019-03-03 18:56:10 +0800

    Changed cp trigger

--- config/general.php
+++ config/general.php
@@ -18,7 +18,7 @@ return [
         'omitScriptNameInUrls' => true,
 
         // Control Panel trigger word
-        'cpTrigger' => 'admin',
+        'cpTrigger' => 'cphy',
 
         // The secure key Craft will use for hashing and encrypting data
         'securityKey' => getenv('SECURITY_KEY'),
